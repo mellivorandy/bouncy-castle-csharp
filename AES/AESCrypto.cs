0d44ee9876f206de2c0bb85dab892970d27bc160
mellivorandy <anydoyi@gmail.com>
2025-02-08 17:20:43 +0800

    Add IAES interface for AES encryption and decryption

---
+++ AES/AESCrypto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace BouncyCastleCSharp.AES
+{
+    internal class AESCrypto : IAES
+    {
+        public byte[] Encrypt(byte[] plainText, byte[] key, byte[] iv)
+        {
+            
+        }
+
+        public byte[] Decrypt(byte[] cipherText, byte[] key, byte[] iv)
+        {
+            
+        }
+    }
+}
---
+++ AES/IAES.cs
@@ -0,0 +1,8 @@
+﻿namespace BouncyCastleCSharp.AES
+{
+    internal interface IAES
+    {
+        byte[] Encrypt(byte[] plainText, byte[] key, byte[] iv);
+        byte[] Decrypt(byte[] cipherText, byte[] key, byte[] iv);
+    }
+}
